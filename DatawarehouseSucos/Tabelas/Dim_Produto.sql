1e7ece663c43c9fa1738aefe5a0e4f827827d80d
gabicantarini <gabilana64@hotmail.com>
2022-01-26 16:20:30 +0000

    feat: new dimensão alteração

---
+++ DatawarehouseSucos/Tabelas/Dim_Produto.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Dim_Produto]
+(
+	[Id] INT NOT NULL , 
+    [Cod_Produto] NVARCHAR(50) NOT NULL, 
+    [Desc_Produto] NVARCHAR(200) NULL, 
+    [Atr_Tamanho] NVARCHAR(200) NULL, 
+    [Atr_Sabor] NVARCHAR(200) NULL, 
+    [Cod_Marca] NVARCHAR(50) NULL, 
+    CONSTRAINT [FK_Dim_Produto_Dim_Marca] FOREIGN KEY ([Cod_Marca]) REFERENCES [Dim_Marca]([Cod_Marca]), 
+    PRIMARY KEY ([Cod_Produto])
+)
